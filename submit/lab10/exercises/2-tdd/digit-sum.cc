aba9d40cda656ec161a875267b16699de6ddbb75
aceylan19 <54588536+aceylan19@users.noreply.github.com>
2020-05-05 18:46:59 -0400

    Updating digit sum
    Added checking changes to the digitSum function.

--- submit/lab10/exercises/2-tdd/digit-sum.cc
+++ submit/lab10/exercises/2-tdd/digit-sum.cc
@@ -7,8 +7,10 @@ int digitSum(const char* str)
   int acc = 0;
 
   for (const char* p = str; *p; ++p) {
-	char c = *p;
+	char c = *p; 
 	if (c != ' ' && c != '+' && (c > '9' || c < '0') ) break;
+	if( (c>'0' || c<'9') && ((char) *(p+1)>'0' || (char) *(p+1)<'9')) break;
+        if( c == '+' && (char) *(p+1) == '+') break;
 	if (isdigit(c)) acc += *p - '0';
    }
 
